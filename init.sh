73a3ae838327167908434351663a7c882cbd7482
rlovelessiii <richardlovelessiii@gmail.com>
2020-09-12 18:23:10 -0400

    Refactored to expand directories in the future

--- init.sh
+++ init.sh
@@ -4,46 +4,55 @@
 # author: RLovelessIII
 # description: Designed to create Symbolic Links for Unix '.' configuration files
 
+# Globals
+BASE_PATHS=(~/.testlinks /etc)
+
 # @Params: $1 = Current File/Dir Name $2 = Path to symbolic link
 # Recursive function to evaluate the contents of the given directory.
 # File vs Directory
-function link_search {
+function link {
     # If the contents are a file, a symbolic link is created
     if [[ -f $1 ]]; then
-        echo "Creating SymLink for $1"
         # $(pwd) returns the current directory, where arg $1 is concatenated to provide the absolute file path,
         #   and arg $2 provides the config directory for the link to be created
-        ln -sf $(pwd)/$1 $2/$1 && echo "Success!"
+        echo "Creating symlink for $2/$1..."
+        ln -sf $(pwd)/$1 $2/$1
     elif [[ -d $1 ]]; then
         cd $1
         # Check if current directory already exists, if not, create it
         if ! [[ -d $2/$1 ]]; then
+            echo "Creating directory $2/$1..."
             mkdir $2/$1
         fi
         # Iterate over the conents of the directory to call itself,
         #   using the current ${ITEM} for arg $1 and concatenating the file path for arg $2
-        for ITEM in *; do
-            link_search ${ITEM} $2/$1
+        for content in *; do
+            link $content $2/$1
         done
         cd ..
     fi
 }
 
+# @Params: $1 = Parent directory to start linking, $2 = Root base path for syslink
+function parent_dir {
+    cd $1
+    for child in *; do
+        link $child $2
+    done
+    cd ..
+}
+
 # Get current directory and operating system
 OS=$(uname)
 
-if [[ ${OS} == "Linux" ]]; then
-    for PARENT in *; do
-        # Allow '*' to include '.' files from here on out
-        shopt -s dotglob
-        if [[ -d ${PARENT} ]]; then
-            cd ${PARENT}
-            for CHILD in *; do
-                link_search ${CHILD} ~/.testlinks
-            done
-            cd ..
-        fi
-    done
-elif [[ ${OS} == "Darwin" ]]; then
+if [[ $OS == "Linux" ]]; then
+    # Allow '*' to include '.' files
+    shopt -s dotglob
+    echo "Linking..."
+    echo "Starting /home directory..."
+    parent_dir ./home ~/.testlinks
+    echo ".../home directory complete!"
+    echo "Linking Complete!"
+elif [[ $OS == "Darwin" ]]; then
     echo "TODO"
 fi
