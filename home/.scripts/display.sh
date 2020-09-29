22115759e11782b7796619e9c339682b72ef9913
rloveles <rloveles@uncc.edu>
2020-09-29 17:24:27 -0400

    Added 21:9 and 11:9 aspect ratio options for PBP

--- home/.scripts/display.sh
+++ home/.scripts/display.sh
@@ -9,19 +9,21 @@ if [[ $(hostname) == "fox" ]]; then
         xrandr --output eDP1 --auto --output HDMI1 --off
     fi
 elif [[ $(hostname) == "knight" ]]; then
-    if [[ $1 == "ultrawide" ]]; then
-        OUTPUT="DP-1"
+    if [[ $1 == "32:9" ]]; then
         WIDTH="5120"
-        HEIGHT="1440"
         RATE="60"
-    elif [[ $1 == "wide" ]]; then
-        OUTPUT="DP-1"
+    elif [[ $1 == "21:9" ]]; then
+        WIDTH="3360"
+        RATE="120"
+    elif [[ $1 == "16:9" ]]; then
         WIDTH="2560"
-        HEIGHT="1440"
+        RATE="120"
+    elif [[ $1 == "11:9" ]]; then
+        WIDTH="1760"
         RATE="120"
     fi
 
-    CMD1="xrandr --output $OUTPUT --mode ${WIDTH}x${HEIGHT} --rate $RATE --pos 0x464"
+    CMD1="xrandr --output DP-1 --mode ${WIDTH}x1440 --rate $RATE --pos 0x464"
 
     if [[ $2 == "external" ]]; then
         CMD2="xrandr --output DP-2 --mode 2560x1440 --rate 165 --pos ${WIDTH}x0 --rotate right"
