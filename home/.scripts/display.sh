f5043440961b0e47ead7918db54e074935cc9767
rloveles <rloveles@uncc.edu>
2020-09-30 07:38:25 -0400

    Refactored for auto adjusting

--- home/.scripts/display.sh
+++ home/.scripts/display.sh
@@ -5,32 +5,15 @@
 if [[ $(hostname) == "fox" ]]; then
     if [[ $1 == "external" ]]; then
         xrandr --output HDMI1 --auto --output eDP1 --off
-    elif [[ $1 == "internal" ]]; then
+    else
         xrandr --output eDP1 --auto --output HDMI1 --off
     fi
 elif [[ $(hostname) == "knight" ]]; then
-    if [[ $1 == "ultrawide" ]]; then
-        WIDTH="5120"
-        RATE="60"
-    elif [[ $1 == "superwide" ]]; then
-        WIDTH="3360"
-        RATE="60"
-    elif [[ $1 == "wide" ]]; then
-        WIDTH="2560"
-        RATE="120"
-    elif [[ $1 == "not-wide" ]]; then
-        WIDTH="1760"
-        RATE="120"
-    fi
+    WIDTH=('5120', '3360', '2560', '1760')
 
-    CMD1="xrandr --output DP-1 --mode ${WIDTH}x1440 --rate $RATE --pos 0x464"
-
-    if [[ $2 == "external" ]]; then
-        CMD2="xrandr --output DP-2 --mode 2560x1440 --rate 165 --pos ${WIDTH}x0 --rotate right"
-    else
-        CMD2="xrandr --output DP-2 --off"
-    fi
-
-    $CMD1 && $CMD2
+    for w in ${WIDTH[@]}; do
+        xrandr --output DP-1 --mode ${w}x1440 --pos 0x464 && \
+            xrandr --ouptput DP-2 --mode 2560x1440 --pos ${w}x0 --rotate right 
+    done
 fi
 
