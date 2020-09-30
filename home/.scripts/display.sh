86480730f04bf927d05973bcf102d172fa238376
rloveles <rloveles@uncc.edu>
2020-09-30 07:51:14 -0400

    Updated external monitor logic

--- home/.scripts/display.sh
+++ home/.scripts/display.sh
@@ -11,9 +11,16 @@ if [[ $(hostname) == "fox" ]]; then
 elif [[ $(hostname) == "knight" ]]; then
     WIDTH=('5120' '3360' '2560' '1760')
 
+    CMD1=()
+    CMD2=() 
+
     for w in ${WIDTH[@]}; do
-        xrandr --output DP-1 --mode ${w}x1440 --pos 0x464 && \
-            xrandr --output DP-2 --mode 2560x1440 --pos ${w}x0 --rotate right 
+        CMD1+=( 'xrandr --output DP-1 --mode ${w}x1440 --pos 0x464' )
+        if [[ $1 == "external" ]]; then
+            CMD2+=( 'xrandr --output DP-2 --mode 2560x1440 --pos ${w}x0 --rotate right' )
+        else
+            CMD2='xrandr --output DP-2 --off'
+        fi
     done
 fi
 
