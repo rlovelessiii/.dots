456e502942bf80aaa683b9eb09f1741edb3fb6e4
rloveles <rloveles@uncc.edu>
2020-10-09 14:40:05 -0400

    Automate display configuration for knight

--- home/.scripts/display.sh
+++ home/.scripts/display.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-# Script to switch between external and internal monitors
+# Author: Richard Loveless III (rlovelessiii)
+# Description: Script to automate display configuration across different machines
 
 if [[ $(hostname) == "fox" ]]; then
     if [[ $1 == "external" ]]; then
@@ -9,21 +10,53 @@ if [[ $(hostname) == "fox" ]]; then
         xrandr --output eDP1 --auto --output HDMI1 --off
     fi
 elif [[ $(hostname) == "knight" ]]; then
-    WIDTHS=('5120' '3360' '2560' '1760')
 
-    for width in ${WIDTHS[@]}; do
-        if [[ xrandr -q | grep " ${width}x1440 " ]]
-            CMD="nvidia-settings --assign CurrentMetaMode=\"DP-2: ${width}x1440_120 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
-            break;
+    MONITORS_CONNECTED=()
+    RESOLUTIONS_SUPPORTED=()
+    RATES_SUPPORTED=()
+    COMMON_RATES=()
+    HORIZONTAL_OFFSET=( 0 )
+    VERTICAL_OFFSET=( 0 0 )
+
+    MONITOR_DATA=( $(xrandr -q | grep -A 1 " connected ") )
+    
+    for data in ${MONITOR_DATA[@]}; do
+        if [[ $data == "connected" ]]; then
+            MONTIORS_CONNECTED+=( ${PREV_DATA} )
+        elif [[ $data == ????x???? ]]; then
+            RESOLUTION_SUPPORTED+=( ${data} )
+        elif [[ $data =~ ^[0-9]{2,3}[\.] ]]; then
+            RATES_SUPPORTED+=( ${data} )
+        else
+            PREV_DATA=${data}
         fi
     done
 
-    if [[ $1 == "external" ]]; then
-        CMD="${CMD}, DP-4: 2560x1440_120 +2560+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}\""
-    else
-        CMD="${CMD}\""
-    fi
+    declare -A RATE
+    
+    for rate in ${RATES_SUPPORTED[@]}; do
+        HZ=$(echo ${rate} | cut -d . -f 1)
+        RATE[${HZ}]=$((RATE[${HZ}]+1))
+        if [[ RATE[${HZ}] -gt 1 ]]; then
+            COMMON_RATES+=( ${HZ} )
+        fi
+    done
 
-    CMD
+    HIGHEST_RATE=0
+    
+    for rate in ${COMMON_RATES[@]}; do
+        if [[ ${rate} -gt ${HIGHEST_RATE} ]]; then
+            HIGHEST_RATE=${rate}
+        fi
+    done
+
+    HORIZONTAL_OFFSET+=( $(echo ${RESOLUTIONS_SUPPORTED[0]} | cut -d x -f 1) )
+
+    for (( i=0; i<${#MONITORS_CONNECTED[@]}; i++ )); do
+        META="${META} ${MONITORS_CONNECTED[i]}: ${RESOLUTIONS_SUPPORTED[i]}_${HIGHEST_RATE} +${HORIZONTAL_OFFSET[i]}+${VERITCAL_OFFSET[i]} {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On},"
+    done
+
+    nvidia-settings --assign CurrentMetaMode="${META}"
+
 fi
 
