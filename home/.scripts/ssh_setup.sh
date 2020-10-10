68b3e0add40e07a2da19b01e50020ca92242e2c2
rlovelessiii <richardlovelessiii@gmail.com>
2020-10-10 10:25:25 -0400

    Update remote automatically

--- home/.scripts/ssh_setup.sh
+++ home/.scripts/ssh_setup.sh
@@ -9,8 +9,8 @@ if [[ ${ID} == "github" ]]; then
     ssh-keygen -t rsa -b 4096 -C "$(git config user.email)" -f ~/.ssh/id_rsa_github -N "" && \
         eval "$(ssh-agent -s)" && \
         ssh-add ~/.ssh/id_rsa_github && \
-        xclip -sel clip < ~/.ssh/id_rsa_github.pub
-    echo "Github ssh-key has been copied to the clipboard..."
-    echo "Opening https://github.com/login"
-    $BROWSER --new-window https://github.com/login & disown
+        cd $HOME/.dots && \
+        git remote set-url origin git@github.com:rlovelessiii/.dots.git && \
+        xclip -sel clip < ~/.ssh/id_rsa_github.pub && \
+        $BROWSER --new-window https://github.com/login & disown
 fi
