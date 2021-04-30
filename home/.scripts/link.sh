748a8346c1ac389e61e351865e74fab806e49294
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-30 16:19:57 -0400

    refactored backup file creation

--- home/.scripts/link.sh
+++ home/.scripts/link.sh
@@ -5,22 +5,22 @@
 function link {
 	# Check if input is a file or a directory
 	if [[ -f "${1}" ]]; then
+		file_path="${2}/${1}"
 		# Check if output location is a file
-		if [[ -f "${2}/${1}" ]]; then
-			echo "Creating backup for ${2}/${1}"
-			mkdir -p ${3}/${1}
-			mv "${2}/${1}" "${3}/${1}.bak"
+		if [[ -f ${file_path} ]]; then
+			echo "Creating backup for ${file_path}"
+			mv "${file_path}" "${file_path}.bak"
 		fi
 		# Confirm output location is not a current symlink
-		if ! [[ -L "${2}/${1}" ]]; then
-			echo "Creating symlin for ${2}/${1}..."
-			ln -s $(pwd)/${1} ${2}/${1}
+		if ! [[ -L "${file_path}" ]]; then
+			echo "Creating symlin for ${file_path}..."
+			ln -s $(pwd)/${1} ${file_path}
 		fi
 	elif [[ -d ${1} ]]; then
 		mkdir -p ${2}/${1}
 		cd ${1}
 		for content in *; do
-			link $content ${2}/${1} ${3}/${1}
+			link $content ${2}/${1}
 		done
 		cd ..
 	fi
@@ -31,8 +31,7 @@ function link {
 #	$2 = Base path for symlinks
 
 PARENT_DIR=${1}
-SYMLINK=${2}
-BACKUP_DIR=${2}/.backups
+SYM_LINK=${2}
 
 echo "Linking ${PARENT_DIR} directory..."
 
@@ -42,7 +41,7 @@ shopt -s dotglob
 cd ${PARENT_DIR}
 
 for child in *; do
-	link ${child} ${SYMLINK} ${BACKUP_DIR}
+	link ${child} ${SYM_LINK}
 done
 
 echo "Linking ${PARENT_DIR} directory...Finished!"
