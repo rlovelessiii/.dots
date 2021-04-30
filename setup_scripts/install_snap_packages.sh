69d7ce60f1e2191d197e9b5df6e555aa43476d55
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-30 17:57:30 -0400

    even more typos fixed

--- setup_scripts/install_snap_packages.sh
+++ setup_scripts/install_snap_packages.sh
@@ -6,7 +6,7 @@ echo "Installing Snap Packages..."
 
 CONF_DIR="$(dirname "$0")/setup_scripts/config"
 
-sudo ln -s /var/lib/snapd/snap /snap
+sudo ln -s /var/lib/snapd/snap /snap && \
 
 while read -r line; do
 	PACKAGE=$(echo ${line} | cut -d \# -f 1)
--- setup_scripts/setup_directories.sh
+++ setup_scripts/setup_directories.sh
@@ -2,7 +2,7 @@
 
 # Author: Richard C. Loveless III (rlovelessiii)
 
-CONF_DIR="$(dirname "$0)/setup_scripts/config/"
+CONF_DIR="$(dirname "$0")/setup_scripts/config"
 
 echo "Creating Directories..."
 
