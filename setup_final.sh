8fc7b216022396aef8a13b0164b064493031b2e1
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-30 17:50:42 -0400

    fix more typos

--- setup_final.sh
+++ setup_final.sh
@@ -4,7 +4,7 @@
 
 echo "Dotfiles Setup Being Finalized..."
 
-SCRIPT_DIR="$(dirname "$0")/home/.scripts/"
+SCRIPT_DIR="$(dirname "$0")/setup_scripts/"
 
 . ${SCRIPT_DIR}/install_snap_packages.sh && \
 . ${SCRIPT_DIR}/setup_directories.sh && \
