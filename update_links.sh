3230ee6d8ce552a9126d41904e71031e5a93c78b
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-29 22:07:52 -0400

    haha

--- update_links.sh
+++ update_links.sh
@@ -6,6 +6,6 @@ echo "Updating Symbolic Links..."
 
 LINK_SCRIPT="$(dirname "$0")/home/.scripts/link.sh"
 
-LINK_SCRIPT ./home $HOME
+${LINK_SCRIPT} ./home $HOME
 
 echo "Updating Symbolic Links...Finished!"
