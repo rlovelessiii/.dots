3b48e516792515e04b239f73cc0d290c438a1b70
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-30 18:31:36 -0400

    update install command

--- setup_scripts/setup_bashit.sh
+++ setup_scripts/setup_bashit.sh
@@ -6,6 +6,6 @@ echo "Installing Bash-It..."
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 
-~/.bash_it/install.sh --append-to-config
+~/.bash_it/install.sh --silent
 
 echo "Installing Bash-It...Finished!"
