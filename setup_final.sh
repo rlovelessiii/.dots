4b8466adfc97fa8f65edbec52c1dd3fd079f0f3c
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-04-30 22:23:10 -0400

    last change i hope

--- setup_final.sh
+++ setup_final.sh
@@ -7,15 +7,9 @@ echo "Dotfiles Setup Being Finalized..."
 DIR_NAME=$(dirname "$0")
 SCRIPT_DIR="${DIR_NAME}/setup_scripts/"
 
-printf "Installing Snap Packages..." && \
-${SCRIPT_DIR}/install_snap_packages.sh > ${SCRIPT_DIR}/install_snap_packages.log && \
-echo "Done!" && \
-printf "Setting Up Directories..." && \
-${SCRIPT_DIR}/setup_directories.sh > ${SCRIPT_DIR}/setup_directories.log && \
-echo "Done!" && \
-printf "Creating Symbolic Links..." && \
-${DIR_NAME}/update_links.sh > ${DIR_NAME}/update_links.log && \
-echo "Done!" && \
+${SCRIPT_DIR}/install_snap_packages.sh && \
+${SCRIPT_DIR}/setup_directories.sh && \
+${DIR_NAME}/update_links.sh && \
 
 echo "Dotfiles Setup Being Finalized...Done!"
 
--- setup_init.sh
+++ setup_init.sh
@@ -7,21 +7,11 @@ SCRIPT_DIR="${DIR_NAME}/setup_scripts"
 
 echo "Dotfiles Setup Initialized..."
 
-printf "Installing DNF Packages..." && \
-${SCRIPT_DIR}/install_dnf_packages.sh > ${SCRIPT_DIR}/install_dnf_packages.log && \
-echo "Done!" && \
-printf "Installing Docker..." && \
-${SCRIPT_DIR}/install_docker.sh > ${SCRIPT_DIR}/install_docker.log && \
-echo "Done!" && \
-printf "Installing Bash-It..." && \
-${SCRIPT_DIR}/install_bashit.sh > ${SCRIPT_DIR}/install_bashit.log && \
-echo "Done!" && \
-printf "Installing SDK-Man..." && \
-${SCRIPT_DIR}/install_sdkman.sh > ${SCRIPT_DIR}/install_sdkman.log && \
-echo "Done!" && \
-printf "Installing NVM..." && \
-${SCRIPT_DIR}/install_nvm.sh > ${SCRIPT_DIR}/install_nvm.log && \
-echo "Done!" && \
+${SCRIPT_DIR}/install_dnf_packages.sh && \
+${SCRIPT_DIR}/install_docker.sh && \
+${SCRIPT_DIR}/install_bashit.sh && \
+${SCRIPT_DIR}/install_sdkman.sh && \
+${SCRIPT_DIR}/install_nvm.sh && \
 
 echo "Dotfiles Setup Initialized...Done!"
 
