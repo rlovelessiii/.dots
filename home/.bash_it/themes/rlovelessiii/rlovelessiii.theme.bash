935f7c14617ccd24c94671898386bf769ff6a8b5
Richard C. Loveless III <richardlovelessiii@gmail.com>
2021-05-13 09:06:09 -0400

    remove bash-it battery plugin and altered cmd prompt

--- home/.bash_it/themes/rlovelessiii/rlovelessiii.theme.bash
+++ home/.bash_it/themes/rlovelessiii/rlovelessiii.theme.bash
@@ -1,8 +1,4 @@
 
-# Battery Plugin Information
-BC=`battery_percentage`
-[[ $BC -lt '0' ]] && BC='100'
-
 # Prompt
 function prompt_command() {
 
@@ -12,20 +8,19 @@ function prompt_command() {
   arrow_color=${bold_yellow}
   line_color=${yellow}
   time_color=${bold_purple}
-  battery_color=${red}
   current_directory_color=${bold_blue}
   prompt_ending_color=${bold_white}
   cmd_color=${bold_white}
   
-  PS1="\n${line_color}╔═${bracket_color}[${username_color}\u${arrow_color} »» ${host_color}\h${bracket_color}]${arrow_color} »» ${bracket_color}[${time_color}\A${bracket_color}]${arrow_color} »» ${bracket_color}(${battery_color}$BC%${bracket_color})\n${line_color}╠═${bracket_color}(${current_directory_color}\w${bracket_color})$(scm_prompt_info)\n${line_color}║\n${line_color}╚═${bracket_color}[${prompt_ending_color}\$${arrow_color} »» ${cmd_color}"
+  PS1="\n${line_color}╔═${bracket_color}[${username_color}\u${arrow_color} »» ${host_color}\h${bracket_color}]${arrow_color} »» ${bracket_color}[${time_color}\A${bracket_color}]\n${line_color}╠═${bracket_color}(${current_directory_color}\w${bracket_color})$(scm_prompt_info)\n${line_color}║\n${line_color}╚═${bracket_color}[${prompt_ending_color}\$${arrow_color} »» ${cmd_color}"
 
 }
 
 # scm theming
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_PREFIX="${bold_yellow} »» ${bold_black}(${bold_cyan}"
-SCM_THEME_PROMPT_SUFFIX="${bold_black})${reset_color}"
+SCM_THEME_PROMPT_PREFIX="\n${yellow}╠═${bold_white}{ ${bold_cyan}"
+SCM_THEME_PROMPT_SUFFIX="${bold_white} }${reset_color}"
 
 safe_append_prompt_command prompt_command
 
